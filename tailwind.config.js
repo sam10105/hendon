3b7639b3f150acc2db8315f9b2b7c525816da806
sam10105 <samkasyoki@gmail.com>
2022-01-25 14:35:22 +0300

    add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    fontFamily: {
+      heebo: ['Heebo', ...defaultTheme.fontFamily.sans],
+      sans: ['Raleway', ...defaultTheme.fontFamily.sans],
+      serif: ['Cormorant Garamond', ...defaultTheme.fontFamily.serif],
+    },
+    extend: {},
+  },
+  plugins: [],
+};
